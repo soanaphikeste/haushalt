68ae9b43e944862a39f0c767d47f436adf8c6c8e
Prior <fgnodtke@cronosx.de>
2014-04-01 09:59:12 +0200

    Changes to style. Color still to be determined

--- webserver.js
+++ webserver.js
@@ -3,32 +3,26 @@ var fs = require("fs"); //Import tools for manipulating filesystem
 var url = require("url"); //Parsing urls
 var mime = require('mime'); //Parsing MIME-Types
 
-function request(req, res) 
-{
+function request(req, res)  {
 	var reqUrl = url.parse(req.url);
 	var path = reqUrl.pathname;
 	if(path == "/") path = "./index.html";
 	else path = "." + path;
-	fs.readFile(path, function(err, data)
-	{
-		if(err)
-		{
+	fs.readFile(path, function(err, data) {
+		if(err) {
 			res.writeHead(404, {"Content-Type" : "text/html"});
 			res.end("Not found");
 			console.log(err);
 		}
-		else
-		{
+		else {
 			res.writeHead(200, {"Content-Type" : mime.lookup(path)});
 			res.end(data);
 		}
 	});
 }
 
-module.exports = 
-{
-	start : function(port) 
-	{
+module.exports = {
+	start : function(port) {
 		var httpserver = http.createServer(request);
 		httpserver.listen(port);
 	}
