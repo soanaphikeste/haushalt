c1083040c20e685c54f690400dae8661ab5174cf
Prior <fgnodtke@cronosx.de>
2014-04-07 14:00:18 +0200

    Changed signature of oncontentattached to onattached and added ondetached event

--- content.js
+++ content.js
@@ -3,6 +3,9 @@ Content =
 	setContent : function(obj)
 	{
 		var content = $('#content');
+		if(this.current !== undefined && this.current.onDetached !== undefined)
+			this.current.onDetached();
+		this.current = obj;
 		content.css({"opacity" : "0"});
 		setTimeout(function()
 		{
@@ -10,8 +13,8 @@ Content =
 			content.append("<h1>" + obj.heading + "</h1>");
 			content.append($("<div id='contentText'></div>").append(obj.html));
 			content.css({"opacity" : "1"});
-			if(obj.onContentAttached !== undefined)
-				obj.onContentAttached();
+			if(obj.onAttached !== undefined)
+				obj.onAttached();
 		}, 200);
 	}
 };
